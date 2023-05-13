bd2204e1f2384dd9e4f0025c4d85428fc565f6c0
Bernadett Ignácz <detti.ignacz@outlook.hu>
2023-05-13 17:35:44 +0200

    update simon.php

---
+++ simon.php
@@ -0,0 +1,15 @@
+<div class="container">
+  <div class="row">
+<p>
+A Simon játék egy elektronikus játék, amelyet 1978-ban adtak ki. A játék koncepciója egyszerű, de szórakoztató: A játékosnak meg kell jegyeznie és ismételnie kell a gép által bemutatott színsorozatot, amely minden egyes körben hosszabbodik. A játék lényege az, hogy a játékosnak minél hosszabb ideig sikerüljön megjegyeznie a színsorozatokat, mielőtt hibázna.</p>
+<p>A játékot Ralph H. Baer találta ki, aki a Magnavox Odyssey konzol megalkotója volt. A játékot Milton Bradley vállalat forgalmazta az Egyesült Államokban, ahol hatalmas sikert aratott, és világszerte számos további verziót készítettek belőle.</p>
+<p>A játékhoz négy színű gombot és egy hanggenerátort használtak, amely a színsorozatokat kísérő hangokat adta ki. A játék a 80-as években volt a csúcspontján, de az évek során sok más verzió is készült belőle, és ma már online is játszható.</p>
+<p>A Simon játék az elektronikus játékok aranykorában született, és hozzájárult ahhoz, hogy az emberek számítógépek és elektronikus játékok iránti érdeklődése növekedjen. Az egyszerű, de szórakoztató játékma adta az inspirációt számos más játékhoz, és ma is népszerű szabadidős tevékenység.</p>
+
+<a href="simon.html">
+        <button type="button" class="btn btn-primary me-3" >
+          Kipróbálom</button>
+        </a>
+</div>
+</div>
+
